1bf7827d2928d00cff3095a8a678be8dca45d474
nishikino25 <cute01jenny@gmail.com>
2021-03-17 11:43:55 +0800

    Add Supplier simple sql.

---
+++ src/main/resources/db/dev/supplierpersonlist.sql
@@ -0,0 +1,11 @@
+
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('葉大雄','經理', '021234567', '001', '123', 'example@ex.com',1);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('王大明','行銷經理', '021234567', '001', '123', 'example@ex.com',1);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('葉大雄','經理', '021234567', '001', '123', 'example@ex.com',2);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('王大明','行銷經理', '021234567', '001', '123', 'example@ex.com',2);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('葉大雄','經理', '021234567', '001', '123', 'example@ex.com',3);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('王大明','行銷經理', '021234567', '001', '123', 'example@ex.com',3);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('葉大雄','經理', '021234567', '001', '123', 'example@ex.com',4);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('王大明','行銷經理', '021234567', '001', '123', 'example@ex.com',4);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('葉大雄','經理', '021234567', '001', '123', 'example@ex.com',5);
+insert into supplier_contact_person (`name`,`position`, `phone`, `ext`, `fax`, `email`,`supplier_id`) values ('王大明','行銷經理', '021234567', '001', '123', 'example@ex.com',5);
