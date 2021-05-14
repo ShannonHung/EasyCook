036770497f9f07a2eeabbc08b6f9b8f62128f8e5
Yi-Hsuan Hung <b10709036@mail.ntust.edu.tw>
2021-05-15 00:22:51 +0800

    remove: db for real demo

--- src/main/resources/db/product/recipe_image.sql
+++ src/main/resources/db/product/recipe_image.sql
@@ -4,18 +4,18 @@ LIMIT 0, 1000
 
 -- Date: 2021-05-12 14:37
 */
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (5,'pexels-ella-olsson-3026808.jpg',NULL,'image/jpeg',5);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (9,'pexels-ella-olsson-1640769.jpg',NULL,'image/jpeg',9);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (17,'pexels-ella-olsson-1640769.jpg',NULL,'image/jpeg',1);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (18,'pexels-cottonbro-3297367.jpg',NULL,'image/jpeg',2);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (21,'pexels-ella-olsson-3026808.jpg',NULL,'image/jpeg',3);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (22,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',4);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (23,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',6);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (24,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',7);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (25,'pexels-ella-olsson-1640769.jpg',NULL,'image/jpeg',8);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (26,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',10);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (27,'pexels-ella-olsson-1640769.jpg',NULL,'image/jpeg',11);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (28,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',12);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (29,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',13);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (30,'pexels-ella-olsson-3026808.jpg',NULL,'image/jpeg',14);
-INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (31,'pexels-cottonbro-3297367.jpg',NULL,'image/jpeg',15);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (17,'pexels-ella-olsson-1640769.jpg',NULL,'image/jpeg',1);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (5,'pexels-ella-olsson-3026808.jpg',NULL,'image/jpeg',5);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (9,'pexels-ella-olsson-1640769.jpg',NULL,'image/jpeg',9);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (18,'pexels-cottonbro-3297367.jpg',NULL,'image/jpeg',2);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (21,'pexels-ella-olsson-3026808.jpg',NULL,'image/jpeg',3);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (22,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',4);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (23,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',6);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (24,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',7);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (25,'pexels-ella-olsson-1640769.jpg',NULL,'image/jpeg',8);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (26,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',10);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (27,'pexels-ella-olsson-1640769.jpg',NULL,'image/jpeg',11);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (28,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',12);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (29,'pexels-geraud-pfeiffer-6605904.jpg',NULL,'image/jpeg',13);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (30,'pexels-ella-olsson-3026808.jpg',NULL,'image/jpeg',14);
+--INSERT INTO recipe_image (`id`,`image_name`,`s3url`,`image_type`,`recipe_id`) VALUES (31,'pexels-cottonbro-3297367.jpg',NULL,'image/jpeg',15);
